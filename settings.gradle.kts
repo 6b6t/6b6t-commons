0e96eee5dac7100858a29fd71fd6208908c29978
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2025-12-31 20:35:30 +0100

    feat: add develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,10 +8,23 @@ pluginManagement {
 }
 
 plugins {
+    id("com.gradle.develocity") version "4.3"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 rootProject.name = "server-commons"
+
+develocity {
+    buildScan {
+        val isCi = !System.getenv("CI").isNullOrEmpty()
+        if (isCi) {
+            termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
+            termsOfUseAgree = "yes"
+            tag("CI")
+        }
+        publishing.onlyIf { isCi }
+    }
+}
 
 include("commons-core")
 include("commons-database")
