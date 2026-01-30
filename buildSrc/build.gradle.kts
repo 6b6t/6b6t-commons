7b41cf331738f5acef26de067681c6509c42943d
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-01-30 09:28:36 +0100

    Upgrade Java and Kotlin toolchain to version 25

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -24,10 +24,10 @@ dependencies {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(25))
     }
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
 }
